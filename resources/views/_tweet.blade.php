143aa0aee42a4ccf9668946ff7b5c57180d79f75
malikalichsan <malik.al.ichsan@gmail.com>
2020-12-28 16:13:28 +0700

    Design the Timeline

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,12 @@
+<div class="flex p-4 border-b border-b-gray">
+    <div class="mr-2 flex-shrink-0 ">
+        <img src="https://i.pravatar.cc/50" class="rounded-full mr-2" alt="">
+    </div>
+
+    <div>
+        <h5 class="font-bold mb-4">John Doe</h5>
+        <p class="text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consequatur itaque iusto quo, sed tempora voluptates! Ad aliquid culpa cum delectus doloremque esse, hic libero nihil rerum suscipit, vel voluptas!
+        </p>
+    </div>
+</div>
