ade29204a71f015cddf2c5d875295b21f9b6fe75
malikalichsan <malik.al.ichsan@gmail.com>
2020-12-29 22:34:45 +0700

    Housekeeping

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,21 @@
 
         <section class="px-32">
             <main class="container mx-auto">
-                @yield('content')
+                <div class="lg:flex lg:justify-between mb-16">
+                    <div class="lg:w-32">
+                        @include ('_sidebar-links')
+                    </div>
+
+                    <div class="lg:flex-1 lg:mx-10" style="max-width: 700px;">
+                        @yield ('content')
+                    </div>
+
+                    <div class="lg:w-1/6">
+                        <div class="bg-blue-100 rounded-lg p-4">
+                            @include ('_friends-list')
+                        </div>
+                    </div>
+                </div>
             </main>
         </section>
     </div>
