80c116112d0efe83937e4a5e53947953d8492aeb
malikalichsan <malik.al.ichsan@gmail.com>
2020-12-31 01:47:17 +0700

    Invokable controller

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::post('/profiles/{user}/follow', 'FollowsController@store')->name('follow');
     Route::get('/profiles/{user}/edit', 'ProfilesController@edit')->middleware('can:edit,user');
     Route::patch('/profiles/{user}', 'ProfilesController@update')->middleware('can:edit,user');
-    Route::get('/explore', 'ExploreController@index')->name('explore');
+    Route::get('/explore', 'ExploreController')->name('explore');
 });
 
 Route::get('/profiles/{user}', 'ProfilesController@show')->name('profile');
